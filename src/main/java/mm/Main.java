ddd514757ef6bda3dee3f3ce5e4cfbc00fd013dd
Star4Fun <schnappvid@gmx.de>
2025-04-23 16:32:47 +0200

    FORKED!

---
+++ src/main/java/mm/Main.java
@@ -0,0 +1,20 @@
+package mm;
+
+import mm.gui.Gui;
+import mm.model.Example;
+
+/**
+ * The common starting point of the GUI.
+ */
+public class Main {
+    /**
+     * The external entry point of the application.
+     * @param args The command line arguments passed to the application.
+     */
+    public static void main(String[] args) {
+        Example example = new Example(0);
+        System.out.println("Starting...");
+        Gui.main(args);
+        System.out.println("Exiting...");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * The main module of the mm application.
+ */
+module mm {
+    requires javafx.controls;
+    requires transitive javafx.graphics;
+    requires jbox2d.library;
+    
+    exports mm.gui;
+}
