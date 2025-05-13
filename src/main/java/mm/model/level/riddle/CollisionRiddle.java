cdef3214db0825d7f8d2fb45bafcaea0bbd08ad8
Moritz Mau <moritzmau@me.com>
2025-05-13 23:32:10 +0200

    Made it wor, refactored and trying to use the ContactListener, but no idea if it works. Solve condition has to be reworked.

--- src/main/java/mm/model/level/riddle/CollisionRiddle.java
+++ src/main/java/mm/model/level/riddle/CollisionRiddle.java
@@ -1,90 +1,122 @@
-// package mm.model.level.riddle;
+package mm.model.level.riddle;
 
-// import mm.model.level.riddle.IRiddle;
-// import mm.model.physics.*;
-// import mm.model.physics.PhysicMathUtils;
-// import mm.io.*;
-// import org.jbox2d.callbacks.ContactImpulse;
-// import org.jbox2d.callbacks.ContactListener;
-// import org.jbox2d.collision.Manifold;
-// import org.jbox2d.dynamics.contacts.Contact;
-// import org.jbox2d.dynamics.Body;
-// import org.jbox2d.dynamics.BodyType;
-// import org.jbox2d.common.Vec2;
+import org.jbox2d.common.Vec2;
+import org.jbox2d.dynamics.*;
 
-// public class CollisionRiddle implements IRiddle {
+import mm.io.SpriteData;
+import mm.model.physics.*;
+import mm.render.Sprite;
+import mm.render.Sprites;
 
-//     private Body ball;
-//     private Body target;
-//     private GameWorld gameWorld;
+import org.jbox2d.callbacks.ContactImpulse;
+import org.jbox2d.callbacks.ContactListener;
+import org.jbox2d.collision.Manifold;
+import org.jbox2d.dynamics.contacts.Contact;
+import org.jbox2d.dynamics.Body;
+import org.jbox2d.dynamics.BodyType;
 
-//     private boolean solved = false;
+public class CollisionRiddle implements IRiddle {
+    private GameObjectCompound collisionCompound;
+    private Body ball;
+    private Body target;
+    private GameWorld gameWorld;
 
-//     @Override
-//     public void initialize(GameWorld welt) {
-//         this.gameWorld = welt;
+    private boolean solved = false;
+    private boolean isSolvedState = false;
+    private float solvedTimer = 0f;
+    private static final float RESET_DELAY = 3f; // Sekunden
 
-//         // Contact Listener hinzufügen
-//         welt.box2dWorld.setContactListener(new ContactListener() {
-//             @Override
-//             public void beginContact(Contact contact) {
-//                 Body a = contact.getFixtureA().getBody();
-//                 Body b = contact.getFixtureB().getBody();
+    @Override
+    public void initialize(GameWorld welt) {
+        this.gameWorld = welt;
+        SpriteData ballSprite = Sprites.BALL.getSpriteData();
+        SpriteData boxSprite = Sprites.BOX.getSpriteData();
+        PhysicInformation ballPhysics = new PhysicInformation(
+                BodyType.DYNAMIC, 0.5f, 0.4f, 0.75f);
+        PhysicInformation boxPhysics = new PhysicInformation(
+                BodyType.DYNAMIC, 0.3f, 0.3f, 0.1f);
+        welt.createRectangleGameObject(PhysicInformation.STATIC_DEFAULT, Sprites.DOMINO.getSpriteData(), 0, 0, 1920, 50);
 
-//                 if ((a == ball && b == target) || (a == target && b == ball)) {
-//                     solved = true;
-//                 }
-//             }
 
-//             @Override public void endContact(Contact contact) {}
-//             @Override public void preSolve(Contact contact, Manifold oldManifold) {}
-//             @Override public void postSolve(Contact contact, ContactImpulse impulse) {}
-//         });
 
-//         SpriteData dummy = new SpriteData();
+        // Contact Listener hinzufügen
+        welt.box2dWorld.setContactListener(new ContactListener() {
+         @Override
+         public void beginContact(Contact contact) {
+             Body a = contact.getFixtureA().getBody();
+             Body b = contact.getFixtureB().getBody();
 
-//         PhysicInformation ballInfo = new PhysicInformation(BodyType.DYNAMIC, 0.4f, 1.0f, 0.2f);
-//         PhysicInformation targetInfo = new PhysicInformation(BodyType.STATIC, 0.4f, 0f, 0.1f);
+             if ((a == ball && b == target) || (a == target && b == ball)) {
+                 solved = true;
+             }
+         }
 
-//         float radius = 20f;
-//         float startX = 100f;
-//         float startY = 200f;
+         @Override public void endContact(Contact contact) {}
+         @Override public void preSolve(Contact contact, Manifold oldManifold) {}
+         @Override public void postSolve(Contact contact, ContactImpulse impulse) {}
+     });
 
-//         // Ziel weiter rechts
-//         float targetX = 400f;
-//         float targetY = 100f;
-//         float targetSize = 40f;
+    float startX = 100f;  // Abstand zur Wand
+    float startY = 1200f;  // Höhe in Pixeln
+    float radius = (5*0.2f)*PhysicMathUtils.ratio;
 
-//         welt.createCircleGameObject(ballInfo, dummy, startX, startY, radius);
-//         welt.createRectangleGameObject(targetInfo, dummy, targetX, targetY, targetSize, targetSize);
+     // Ziel weiter rechts
+    float targetX = 600f;
+    float targetY = 100f;
+    float targetSize = 100f;
 
-//         var data = welt.getEntityData();
-//         ball = data.get(data.size() - 2).getGameObject().getBody();
-//         target = data.get(data.size() - 1).getGameObject().getBody();
-//     }
+    welt.createCircleGameObject(ballPhysics, ballSprite, startX, startY, radius);
+    welt.createRectangleGameObject(boxPhysics, boxSprite, targetX, targetY, targetSize, targetSize);
+    welt.createRectangleGameObject(PhysicInformation.STATIC_DEFAULT, Sprites.DOMINO.getSpriteData(), 50, 150, 50f, 200f, 45f);
 
-//     @Override
-//     public void update(float zeitDelta) {
-//         // gameWorld.box2dWorld.step(zeitDelta, 6, 2);
-//     }
 
-//     @Override
-//     public boolean isSolved() {
-//         return solved;
-//     }
+//     var data = welt.getEntityData();
+//     ball = data.get(data.size() - 2).getGameObject().getBody();
+//     target = data.get(data.size() - 1).getGameObject().getBody();
+    }
 
-//     @Override
-//     public void reset() {
-//         solved = false;
-//         ball.setTransform(
-//                 new Vec2(
-//                         PhysicMathUtils.pixelToMeter(100f),
-//                         PhysicMathUtils.pixelToMeter(200f)
-//                 ),
-//                 0f
-//         );
-//         ball.setLinearVelocity(new Vec2(0f, 0f));
-//         ball.setAngularVelocity(0f);
-//     }
-// }
+    @Override
+    public void update(float zeitDelta) {
+        // gameWorld.box2dWorld.step(zeitDelta, 6, 2);
+        if (!isSolvedState && isSolved()) {
+            isSolvedState = true;
+            solvedTimer = RESET_DELAY;
+            System.out.println("Puzzle solved! Resetting in " + RESET_DELAY + " seconds...");
+        }
+
+        if (isSolvedState) {
+            solvedTimer -= zeitDelta;
+            if (solvedTimer <= 0f) {
+                reset();
+                isSolvedState = false;
+                System.out.println("Puzzle has been reset.");
+            }
+        }
+    }
+
+    @Override
+    public boolean isSolved() {
+
+
+        return solved;
+    }
+
+    @Override
+    public void reset() {
+        solved = false;
+        ball.setTransform(
+             new Vec2(
+                     PhysicMathUtils.pixelToMeter(100f),
+                     PhysicMathUtils.pixelToMeter(200f)
+             ),
+             0f
+     );
+        ball.setLinearVelocity(new Vec2(0f, 0f));
+        ball.setAngularVelocity(0f);
+    }
+
+    @Override
+    public Sprite getBackground() {
+        return Sprites.INDUSTRIAL_BACKGROUND.getSprite();}
+}
 
