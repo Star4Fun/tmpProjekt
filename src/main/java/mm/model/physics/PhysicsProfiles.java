5d2caea4de2a1e9729e48a23a90c2db60dbcc8d7
Moritz Mau <moritzmau@me.com>
2025-05-14 09:20:59 +0200

    Update PhysicInformation definitions to include optional dimensions for game objects. Added some parameters for Plank, Gear and the Balls.

--- src/main/java/mm/model/physics/PhysicsProfiles.java
+++ src/main/java/mm/model/physics/PhysicsProfiles.java
@@ -12,27 +12,27 @@ public class PhysicsProfiles {
     );
 
     public static final PhysicInformation BALL = new PhysicInformation(
-            BodyType.DYNAMIC, 0.5f, 0.4f, 0.75f
+            BodyType.DYNAMIC, 0.5f, 0.4f, 0.75f, (5*0.2f)*PhysicMathUtils.ratio
     );
 
     public static final PhysicInformation LIGHT_BALL = new PhysicInformation(
-            BodyType.DYNAMIC, 0.4f, 0.2f, 0.8f
+            BodyType.DYNAMIC, 0.4f, 0.2f, 0.8f, (5*0.067f)*PhysicMathUtils.ratio
     );
 
     public static final PhysicInformation HEAVY_BALL = new PhysicInformation(
-            BodyType.DYNAMIC, 0.3f, 4.0f, 0.1f
+            BodyType.DYNAMIC, 0.3f, 4.0f, 0.1f, (5*0.14f)*PhysicMathUtils.ratio
     );
 
     public static final PhysicInformation BOX = new PhysicInformation(
-            BodyType.DYNAMIC, 0.0f, 0.2f, 0.01f
+            BodyType.DYNAMIC, 0.0f, 0.2f, 0.01f, 256f, 256f
     );
 
     public static final PhysicInformation PLANK = new PhysicInformation(
-            BodyType.STATIC, 0.0f, 0.0f, 0.1f
+            BodyType.STATIC, 0.0f, 0.0f, 0.1f, 200f, 50f
     );
 
     public static final PhysicInformation GEAR = new PhysicInformation(
-            BodyType.DYNAMIC, 0.0f, 0.0f, 0.1f
+            BodyType.STATIC, 0.0f, 0.0f, 0.1f, (5*0.1f)*PhysicMathUtils.ratio
     );
 
 }
